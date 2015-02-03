b8a34f81e1bc0be3d5d4e02b681aae291519ee94
tomafi <tomas.brejla@ness.com>
2015-02-03 09:38:56 +0100

    - added dry run (100 iterations) before measured test is executed (100 iterations)
    - added one more template, this one is not using th:remove

--- src/test/java/TestPerfProblem.java
+++ src/test/java/TestPerfProblem.java
@@ -1,23 +1,43 @@
 import junit.framework.TestCase;
 import net.brdloush.thymeleaf.perfissue.Feature;
+import net.brdloush.thymeleaf.perfissue.FeatureGroup;
 import net.brdloush.thymeleaf.perfissue.Model;
 import net.brdloush.thymeleaf.perfissue.Phone;
-import net.brdloush.thymeleaf.perfissue.FeatureGroup;
+
 import org.springframework.util.StopWatch;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.context.IContext;
-import org.thymeleaf.spring3.SpringTemplateEngine;
 import org.thymeleaf.standard.StandardDialect;
-import org.thymeleaf.standard.expression.StandardExpressionParser;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class TestPerfProblem extends TestCase {
 
 
+	private TemplateEngine getInitializedEngine() {
+        // initialize thymeleaf 2.1.4 + Spring 3.2 Template Engine
+        TemplateEngine templateEngine = new TemplateEngine();
+        StandardDialect standardDialect = (StandardDialect) templateEngine.getDialects().iterator().next();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setTemplateMode("HTML5");
+        resolver.setSuffix(".html");
+        templateEngine.setTemplateResolver(resolver);
+
+        return templateEngine;
+	}
     public void testPerfIssue() {
 
-        // prepare dummy model -> just linked POJOs: 30 phones, each has 5 feature groups, each group has 10 features.
+    	TemplateEngine initializedEngine = getInitializedEngine();
+    	runTest("DRY RUN", "thymeleaf-template", initializedEngine, 100);
+    	runTest("MEASURING RUN", "thymeleaf-template", initializedEngine, 100);
+    	
+    	runTest("DRY RUN", "thymeleaf-template-noremove", initializedEngine,100);
+    	runTest("MEASURING RUN", "thymeleaf-template-noremove", initializedEngine,100);
+    	
+    }
+
+	private void runTest(String testName, String template, TemplateEngine templateEngine, int runTestIterations) {
+		  // prepare dummy model -> just linked POJOs: 30 phones, each has 5 feature groups, each group has 10 features.
         int numPhones = 30;
         int numFeatureGroupsPerPhone = 5;
         int numFeaturesInGroup = 10;
@@ -37,33 +57,25 @@ public class TestPerfProblem extends TestCase {
         }
 
 
-        // initialize thymeleaf 2.1.4 + Spring 3.2 Template Engine
-        TemplateEngine templateEngine = new SpringTemplateEngine();
-        StandardDialect standardDialect = (StandardDialect) templateEngine.getDialects().iterator().next();
-
-        StandardExpressionParser expParser = (StandardExpressionParser) standardDialect.getExpressionParser();
-        System.out.println(expParser.getClass().getName());
-        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
-        resolver.setTemplateMode("HTML5");
-        resolver.setSuffix(".html");
-        templateEngine.setTemplateResolver(resolver);
-        IContext context = new Context();
 
         // feed dummy model to context
+        IContext context = new Context();
         context.getVariables().put("model", model);
 
         // perform tests
-        StopWatch sw = new StopWatch();
+        StopWatch sw = new StopWatch(testName);
 
+        String processed = "";
+        
         sw.start();
-        int runTimes = 10;
-        for (int i=0;i<runTimes;i++) {
-            String processed = templateEngine.process("thymeleaf-template", context);
+        for (int i=0;i<runTestIterations;i++) {
+        	processed = templateEngine.process(template, context);
         }
         sw.stop();
 
         // printout results
+        System.out.println("*** "+testName);
         System.out.println("Total time spent processing "+ sw.getTotalTimeMillis()+" ms");
-        System.out.println("Processing of single page takes "+ (sw.getTotalTimeMillis()/runTimes)+" ms");
-    }
+        System.out.println("Processing of single page takes "+ (sw.getTotalTimeMillis()/runTestIterations)+" ms");
+	}
 }
